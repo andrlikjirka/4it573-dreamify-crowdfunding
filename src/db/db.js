bca602f6093fdf8ce0307f01786f86a1f189bc4b
andrlikjirka <andrlik.jirka@gmail.com>
2024-05-16 01:33:24 +0200

    db user password env

--- src/db/db.js
+++ src/db/db.js
@@ -1,16 +1,13 @@
 import mongoose from "mongoose";
 
-const user = 'and'
-const password = 'and'
-
 const MONGODB_URL = `mongodb://127.0.0.1:27017`;
 
 export const connectDB = async () => {
 
     try {
         await mongoose.connect(MONGODB_URL, {
-            user: user,
-            pass: password,
+            user: process.env.DB_USER,
+            pass: process.env.DB_PASSWORD,
             dbName: 'crowdfunding'
         });
         console.log('Connected to database');
